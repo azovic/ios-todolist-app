16ca4f5c7bd687419ef30559420b6b078606df54
azracerenert <ozerdeniz759@gmail.com>
2026-01-19 01:11:59 +0300

    Initial Commit

---
+++ todolist/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  todolist
+//
+//  Created by Azra Ceren Ertürk on 19.01.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ todolistTests/todolistTests.swift
@@ -0,0 +1,17 @@
+//
+//  todolistTests.swift
+//  todolistTests
+//
+//  Created by Azra Ceren Ertürk on 19.01.2026.
+//
+
+import Testing
+@testable import todolist
+
+struct todolistTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
